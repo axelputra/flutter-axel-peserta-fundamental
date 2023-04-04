d13b7fe276983d6870ce42a4191b0e773af67abc
axelputra <103884034+axelputra@users.noreply.github.com>
2023-04-04 21:27:05 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,81 +3,126 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
       title: 'Calculator',
-      // Application theme data, you can set the colors for the application as
-      // you want
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
       ),
-      // A widget which will be started on application startup
       home: MyHomePage(title: 'Calculator'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   final String title;
   const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController firstNumberController = TextEditingController();
+  TextEditingController secondNumberController = TextEditingController();
+  int result = 0;
+
+  void addNumbers() {
+    setState(() {
+      int firstNumber = int.parse(firstNumberController.text);
+      int secondNumber = int.parse(secondNumberController.text);
+      result = firstNumber + secondNumber;
+    });
+  }
+
+  void subtractNumbers() {
+    setState(() {
+      int firstNumber = int.parse(firstNumberController.text);
+      int secondNumber = int.parse(secondNumberController.text);
+      result = firstNumber - secondNumber;
+    });
+  }
+
+  void multiplyNumbers() {
+    setState(() {
+      int firstNumber = int.parse(firstNumberController.text);
+      int secondNumber = int.parse(secondNumberController.text);
+      result = firstNumber * secondNumber;
+    });
+  }
+
+  void divideNumbers() {
+    setState(() {
+      int firstNumber = int.parse(firstNumberController.text);
+      int secondNumber = int.parse(secondNumberController.text);
+      result = firstNumber ~/ secondNumber;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Text(
-              'Result = 0',
-              textAlign: TextAlign.left,
-              textDirection: TextDirection.ltr,
+              'Result = $result',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+                textAlign: TextAlign.left,
+                textDirection: TextDirection.ltr,   
             ),
             SizedBox(height: 16), // add vertical space of 16 pixels
-            TextField(
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+              controller: firstNumberController,
+              textAlign: TextAlign.center,
               decoration: InputDecoration(
-                hintText: 'Enter first number',
+              border: OutlineInputBorder(),
+              hintText: 'Enter first number',
+                ),
               ),
             ),
             SizedBox(height: 16), // add vertical space of 16 pixels
-            TextField(
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+              controller: secondNumberController,
+              textAlign: TextAlign.center,
               decoration: InputDecoration(
-                hintText: 'Enter last number',
+              border: OutlineInputBorder(),
+              hintText: 'Enter second number',
+                ),
               ),
             ),
             SizedBox(height: 16), // add vertical space of 16 pixels
             ElevatedButton(
-              onPressed: () {
-                // Action when button is pressed
-              },
+              onPressed: addNumbers,
               child: Text('Add'),
             ),
             SizedBox(height: 16), // add vertical space of 16 pixels
             ElevatedButton(
-              onPressed: () {
-                // Action when button is pressed
-              },
-              child: Text('Substract'),
+              onPressed: subtractNumbers,
+              child: Text('Subtract'),
             ),
             SizedBox(height: 16), // add vertical space of 16 pixels
             ElevatedButton(
-              onPressed: () {
-                // Action when button is pressed
-              },
+              onPressed: multiplyNumbers,
               child: Text('Multiply'),
             ),
             SizedBox(height: 16), // add vertical space of 16 pixels
             ElevatedButton(
-              onPressed: () {
-                // Action when button is pressed
-              },
+              onPressed: divideNumbers,
               child: Text('Divide'),
             ),
           ],
